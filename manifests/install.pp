bcf6154cf1c03377c8807dff6293e4da7f2b1be6
Craig Watson <craig@cwatson.org>
2016-04-27 22:58:00 +0100

    gah

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,7 +6,7 @@ class clamav::install {
   exec { 'freshclam-init':
     command     => '/usr/bin/freshclam --quiet > /dev/null 2>&1',
     creates     => '/var/lib/clamav/main.cvd',
-    require     => File['/etc/clamav/freshclam.conf],
+    require     => File['/etc/clamav/freshclam.conf'],
     refreshonly => true,
   }
 }
