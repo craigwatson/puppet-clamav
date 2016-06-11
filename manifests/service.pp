70709968d7007925b1d8d4119a4aa534fb7c8a75
Craig Watson <craig@cwatson.org>
2016-06-12 00:51:21 +0100

    Enable services

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,7 @@
 class clamav::service {
   service { ['clamav-daemon','clamsmtp']:
     ensure     => running,
+    enable     => true,
     hasrestart => true,
     hasstatus  => true,
     require    => File['/etc/clamav/clamd.conf','/etc/clamsmtpd.conf'],
