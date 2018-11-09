6b1a1f21da9f1915abc68fffd3aee3148d9c1a55
Craig Watson <craig.watson@claranet.uk>
2018-11-09 16:32:38 +0000

    Freshclam resource ordering

--- manifests/freshclam.pp
+++ manifests/freshclam.pp
@@ -8,25 +8,28 @@ class clamav::freshclam {
     require => Package['clamav-freshclam'];
   }
 
+  if $::clamav::run_freshclam == true {
+    exec { 'freshclam-init':
+      command => '/usr/bin/freshclam --quiet > /dev/null 2>&1',
+      creates => '/var/lib/clamav/main.cvd',
+      require => File['/etc/clamav/freshclam.conf'],
+      before  => Service['clamav-daemon'],
+    }
+
+    $cron_require = Exec['freshclam-init']
+
+    Service <| title == 'clamav-daemon' |> {
+      require => Cron['freshclam'],
+    }
+  } else {
+    $cron_require = File['/etc/clamav/freshclam.conf']
+  }
+
   cron { 'freshclam':
     ensure  => present,
     command => '/usr/bin/freshclam --quiet > /dev/null 2>&1',
     user    => 'root',
     minute  => '0',
     hour    => '1',
-    require => [File['/etc/clamav/freshclam.conf'],Package['clamav-freshclam']],
   }
-
-  if $::clamav::run_freshclam == true {
-    exec { 'freshclam-init':
-      command     => '/usr/bin/freshclam --quiet > /dev/null 2>&1',
-      creates     => '/var/lib/clamav/main.cvd',
-      require     => [File['/etc/clamav/freshclam.conf'],Service['clamav-daemon']],
-    }
-
-    File['/etc/clamav/freshclam.conf']{
-      notify => Exec['freshclam-init'],
-    }
-  }
-
 }
